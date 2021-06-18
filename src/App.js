ec7f8b135d9d92086fd4ae9b1b1d105ae2332460
007Abdullah <kb337137@gmail.com>
2021-06-18 20:28:13 +0500

    animation

--- src/App.js
+++ src/App.js
@@ -1,10 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import { React, useState } from 'react'
+// Global Style
+import GlobalStyle from './components/GlobalStyle'
+import "./styles.css";
+// import pages
+import AboutSection from './components/AboutSection'
+import ContactMe from './pages/ContactMe'
+import Projects from './pages/Projects'
+import MovieDetail from './pages/MovieDetail'
+// Import navbar
+import { Navbar } from './components/nav/Navbar';
+// Router
+import { Switch, Route, useLocation } from 'react-router-dom'
+// import animate
+import { AnimatePresence } from 'framer-motion'
 
 function App() {
+  const location = useLocation()
+  const [navStatus, setNavStatus] = useState(0)
+  console.log(navStatus)
   return (
-    <div className="App">
-    
+    <div className="App" >
+      <GlobalStyle navStatus={navStatus} />
+      <Navbar navStatus={navStatus} setNavStatus={setNavStatus} />
+      <AnimatePresence exitBeforeEnter>
+        <Switch location={location} key={location.pathname}>
+          <Route path="/" exact>
+            <AboutSection />
+          </Route>
+          <Route path="/projects" exact>
+            <Projects />
+          </Route>
+          <Route path="/work/:id">
+            <MovieDetail />
+          </Route>
+          <Route path="/contact">
+            <ContactMe />
+          </Route>
+        </Switch>
+      </AnimatePresence>
     </div>
   );
 }
